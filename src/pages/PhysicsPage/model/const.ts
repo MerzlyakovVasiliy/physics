186fd4b79e6813a2579b666e5d6f92ed0b046175
Василий Мерзляков <102283648+MerzlyakovVasiliy@users.noreply.github.com>
2024-11-20 09:43:59 +0500

    fix and add formulas

--- src/pages/PhysicsPage/model/const.ts
+++ src/pages/PhysicsPage/model/const.ts
@@ -26,6 +26,53 @@ import restEnergy from '@/assets/stoFundamentals/restEnergy.png'
 import e_free from '@/assets/stoFundamentals/e_free.png'
 import particleMomentum from '@/assets/stoFundamentals/particleMomentum.png'
 import massEnergy from '@/assets/stoFundamentals/massEnergy.png'
+import momentum from '@/assets/conservationLaws/momentum.png'
+import momentumChange from '@/assets/conservationLaws/momentumChange.png'
+import mechanicalWork from '@/assets/conservationLaws/mechanicalWork.png'
+import mechanicalPower from '@/assets/conservationLaws/mechanicalPower.png'
+import kineticEnergy from '@/assets/conservationLaws/kineticEnergy.png'
+import kineticEnergyChange from '@/assets/conservationLaws/kineticEnergyChange.png'
+import potentialEnergyChange from '@/assets/conservationLaws/potentialEnergyChange.png'
+import potentialEnergy from '@/assets/conservationLaws/potentialEnergy.png'
+import elasticPotentialEnergy from '@/assets/conservationLaws/elasticPotentialEnergy.png'
+import totalMechanicalEnergy from '@/assets/conservationLaws/totalMechanicalEnergy.png'
+import energyConservation from '@/assets/conservationLaws/energyConservation.png'
+import velocity from '@/assets/kinematicsMaterialPoint/velocity.png'
+import v_total from '@/assets/kinematicsMaterialPoint/v_total.png'
+import acceleration from '@/assets/kinematicsMaterialPoint/acceleration.png'
+import coordinateAtTime from '@/assets/kinematicsMaterialPoint/coordinateAtTime.png'
+import pruDDisplacement from '@/assets/kinematicsMaterialPoint/pruDDisplacement.png'
+import pruDVelocity from '@/assets/kinematicsMaterialPoint/pruDVelocity.png'
+import pruDAccelerationDisplacement from '@/assets/kinematicsMaterialPoint/pruDAccelerationDisplacement.png'
+import pathWithConstantAcceleration from '@/assets/kinematicsMaterialPoint/pathWithConstantAcceleration.png'
+import projectilePosition from '@/assets/kinematicsMaterialPoint/projectilePosition.png'
+import projectileSpeedX from '@/assets/kinematicsMaterialPoint/projectileSpeedX.png'
+import angularVelocity from '@/assets/kinematicsMaterialPoint/angularVelocity.png'
+import linearAngularVelocityRelation from '@/assets/kinematicsMaterialPoint/linearAngularVelocityRelation.png'
+import centripetalAcceleration from '@/assets/kinematicsMaterialPoint/centripetalAcceleration.png'
+import coulombForce from '@/assets/electrostatics/coulombForce.png'
+import electricFieldStrength from '@/assets/electrostatics/electricFieldStrength.png'
+import pointChargeFieldStrength from '@/assets/electrostatics/pointChargeFieldStrength.png'
+import electricPotential from '@/assets/electrostatics/electricPotential.png'
+import potentialDifference from '@/assets/electrostatics/potentialDifference.png'
+import fieldPotentialRelation from '@/assets/electrostatics/fieldPotentialRelation.png'
+import superpositionPrinciple from '@/assets/electrostatics/superpositionPrinciple.png'
+import capacitorCapacitance from '@/assets/electrostatics/capacitorCapacitance.png'
+import capacitorPlateCapacitance from '@/assets/electrostatics/capacitorPlateCapacitance.png'
+import totalCapacitanceParallel from '@/assets/electrostatics/totalCapacitanceParallel.png'
+import seriesCapacitance from '@/assets/electrostatics/seriesCapacitance.png'
+import capacitorEnergy from '@/assets/electrostatics/capacitorEnergy.png'
+import electricCurrent from '@/assets/lawsDirectCurrent/electricCurrent.png'
+import ohmsLawForSection from '@/assets/lawsDirectCurrent/ohmsLawForSection.png'
+import resistivity from '@/assets/lawsDirectCurrent/resistivity.png'
+import sourceEmf from '@/assets/lawsDirectCurrent/sourceEmf.png'
+import ohmsLawForCompleteCircuit from '@/assets/lawsDirectCurrent/ohmsLawForCompleteCircuit.png'
+import parallelConnection from '@/assets/lawsDirectCurrent/parallelConnection.png'
+import seriesConnection from '@/assets/lawsDirectCurrent/seriesConnection.png'
+import electricWork from '@/assets/lawsDirectCurrent/electricWork.png'
+import jouleLenzLaw from '@/assets/lawsDirectCurrent/jouleLenzLaw.png'
+import electricPower from '@/assets/lawsDirectCurrent/electricPower.png'
+import sourcePower from '@/assets/lawsDirectCurrent/sourcePower.png'
 
 // Массивы для категорий
 const staticsAndHydrostatics = [
@@ -147,12 +194,263 @@ const stoFundamentals = [
     },
 ]
 
+const conservationLaws = [
+    {
+        id: 29,
+        title: "Импульс тела",
+        img: momentum
+    },
+    {
+        id: 30,
+        title: "Закон изменения и сохранения импульса",
+        img: momentumChange
+    },
+    {
+        id: 31,
+        title: "Механическая работа",
+        img: mechanicalWork
+    },
+    {
+        id: 32,
+        title: "Механическая мощность",
+        img: mechanicalPower
+    },
+    {
+        id: 33,
+        title: "Кинетическая энергия тела",
+        img: kineticEnergy
+    },
+    {
+        id: 34,
+        title: "Теорема об изменении кинетической энергии",
+        img: kineticEnergyChange
+    },
+    {
+        id: 35,
+        title: "Теорема об изменении потенциальной энергии",
+        img: potentialEnergyChange
+    },
+    {
+        id: 36,
+        title: "Потенциальная энергия в поле тяжести",
+        img: potentialEnergy
+    },
+    {
+        id: 37,
+        title: "Потенциальная энергия упруго деформированного тела",
+        img: elasticPotentialEnergy
+    },
+    {
+        id: 38,
+        title: "Полная механическая энергия",
+        img: totalMechanicalEnergy
+    },
+    {
+        id: 39,
+        title: "Закон изменения и сохранения механической энергии",
+        img: energyConservation
+    },
+]
+
+const kinematicsMaterialPoint = [
+    {
+        id: 40,
+        title: "Скорость материальной точки",
+        img: velocity
+    },
+    {
+        id: 41,
+        title: "Закон сложения скоростей",
+        img: v_total
+    },
+    {
+        id: 24,
+        title: "Ускорение материальной точки",
+        img: acceleration
+    },
+    {
+        id: 43,
+        title: "Координата при равномерном прямолинейном движении",
+        img: coordinateAtTime
+    },
+    {
+        id: 44,
+        title: "Координата при прямолинейном равноускоренном движении (ПРУД)",
+        img: pruDDisplacement
+    },
+    {
+        id: 45,
+        title: "Скорость при ПРУД",
+        img: pruDVelocity
+    },
+    {
+        id: 46,
+        title: "Связь ускорения с перемещением при ПРУД",
+        img: pruDAccelerationDisplacement
+    },
+    {
+        id: 47,
+        title: "Путь при движении в одном направлении с постоянным ускорением",
+        img: pathWithConstantAcceleration
+    },
+    {
+        id: 48,
+        title: "Координаты тела при движении тела, брошенного под углом к горизонту",
+        img: projectilePosition
+    },
+    {
+        id: 49,
+        title: "Проекция скорости тела на координатные оси при движении тела, брошенного под углом к горизонту",
+        img: projectileSpeedX
+    },
+    {
+        id: 50,
+        title: "Угловая скорость при равномерном движении по окружности",
+        img: angularVelocity
+    },
+    {
+        id: 51,
+        title: "Связь линейной и угловой скоростей",
+        img: linearAngularVelocityRelation
+    },
+    {
+        id: 52,
+        title: "Центростремительное ускорение",
+        img: centripetalAcceleration
+    },
+]
+
+const electrostatics = [
+    {
+        id: 53,
+        title: "Закон Кулона",
+        img: coulombForce
+    },
+    {
+        id: 54,
+        title: "Напряженность электрического поля",
+        img: electricFieldStrength
+    },
+    {
+        id: 55,
+        title: "Напряженность поля точечного заряда",
+        img: pointChargeFieldStrength
+    },
+    {
+        id: 56,
+        title: "Потенциал электростатического поля",
+        img: electricPotential
+    },
+    {
+        id: 57,
+        title: "Разность потенциалов и напряжение",
+        img: potentialDifference
+    },
+    {
+        id: 58,
+        title: "Связь напряжённости поля и разности потенциалов",
+        img: fieldPotentialRelation
+    },
+    {
+        id: 59,
+        title: "Принцип суперпозиции электрических полей",
+        img: superpositionPrinciple
+    },
+    {
+        id: 60,
+        title: "Электроёмкость конденсатора",
+        img: capacitorCapacitance
+    },
+    {
+        id: 61,
+        title: "Электроёмкость плоского конденсатора",
+        img: capacitorPlateCapacitance
+    },
+    {
+        id: 62,
+        title: "Параллельное соединение конденсаторов",
+        img: totalCapacitanceParallel
+    },
+    {
+        id: 63,
+        title: "Последовательное соединение конденсаторов",
+        img: seriesCapacitance
+    },
+    {
+        id: 64,
+        title: "Энергия заряженного конденсатора",
+        img: capacitorEnergy
+    },
+]
+
+const lawsDirectCurrent = [
+    {
+        id: 65,
+        title: "Сила тока",
+        img: electricCurrent
+    },
+    {
+        id: 66,
+        title: "Закон Ома для участка цепи",
+        img: ohmsLawForSection
+    },
+    {
+        id: 67,
+        title: "Удельное сопротивление вещества",
+        img: resistivity
+    },
+    {
+        id: 68,
+        title: "ЭДС источника тока",
+        img: sourceEmf
+    },
+    {
+        id: 69,
+        title: "Закон Ома для полной (замкнутой) электрической цепи",
+        img: ohmsLawForCompleteCircuit
+    },
+    {
+        id: 70,
+        title: "Параллельное соединение проводников",
+        img: parallelConnection
+    },
+    {
+        id: 71,
+        title: "Последовательное соединение проводников",
+        img: seriesConnection
+    },
+    {
+        id: 72,
+        title: "Работа электрического тока",
+        img: electricWork
+    },
+    {
+        id: 73,
+        title: "Закон Джоуля – Ленца",
+        img: jouleLenzLaw
+    },
+    {
+        id: 74,
+        title: "Мощность электрического тока",
+        img: electricPower
+    },
+    {
+        id: 75,
+        title: "Мощность источника тока",
+        img: sourcePower
+    },
+]
+
 // Список всех формул (объединение категорий)
 const allFormulas = [
     ...staticsAndHydrostatics,
     ...magneticField,
     ...electromagneticVibrationsAndWaves,
-    ...dynamics
+    ...dynamics,
+    ...conservationLaws,
+    ...kinematicsMaterialPoint,
+    ...electrostatics,
+    ...lawsDirectCurrent
 ];
 
 // Экспорт категорий
@@ -162,5 +460,9 @@ export const formulas = {
     "Магнитное поле": magneticField,
     "Электромагнитные колебания и волны": electromagneticVibrationsAndWaves,
     "Динамика": dynamics,
-    "Основы специальной теории относительности (СТО)": stoFundamentals
+    "Основы специальной теории относительности (СТО)": stoFundamentals,
+    "Законы сохранения в механике": conservationLaws,
+    "Кинематика материальной точки": kinematicsMaterialPoint,
+    "Электростатика": electrostatics,
+    "Законы постоянного тока": lawsDirectCurrent
 };
