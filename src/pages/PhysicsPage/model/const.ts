9e3ee79ef5181eea45a0778c978fa8506750d78e
Василий Мерзляков <102283648+MerzlyakovVasiliy@users.noreply.github.com>
2024-11-22 12:51:08 +0500

    add and fix formulas

--- src/pages/PhysicsPage/model/const.ts
+++ src/pages/PhysicsPage/model/const.ts
@@ -73,21 +73,117 @@ import electricWork from '@/assets/lawsDirectCurrent/electricWork.png'
 import jouleLenzLaw from '@/assets/lawsDirectCurrent/jouleLenzLaw.png'
 import electricPower from '@/assets/lawsDirectCurrent/electricPower.png'
 import sourcePower from '@/assets/lawsDirectCurrent/sourcePower.png'
+import substanceAmount from '@/assets/molecularKineticTheory/substanceAmount.png'
+import mktEquation from '@/assets/molecularKineticTheory/mktEquation.png'
+import averageTranslationalEnergy from '@/assets/molecularKineticTheory/averageTranslationalEnergy.png'
+import boltzmannEquation from '@/assets/molecularKineticTheory/boltzmannEquation.png'
+import mendeleevClapeyronEquation from '@/assets/molecularKineticTheory/mendeleevClapeyronEquation.png'
+import internalEnergyMonoatomicGas from '@/assets/molecularKineticTheory/internalEnergyMonoatomicGas.png'
+import daltonLaw from '@/assets/molecularKineticTheory/daltonLaw.png'
+import isothermalProcess from '@/assets/molecularKineticTheory/isothermalProcess.png'
+import isochoricProcess from '@/assets/molecularKineticTheory/isochoricProcess.png'
+import isobaricProcess from '@/assets/molecularKineticTheory/isobaricProcess.png'
+import relativeHumidity from '@/assets/molecularKineticTheory/relativeHumidity.png'
+import specificHeatCapacity from '@/assets/thermodynamics/specificHeatCapacity.png'
+import specificHeatOfFusion from '@/assets/thermodynamics/specificHeatOfFusion.png'
+import lambdaVaporization from '@/assets/thermodynamics/lambdaVaporization.png'
+import specificHeatOfCombustion from '@/assets/thermodynamics/specificHeatOfCombustion.png'
+import isobaricWork from '@/assets/thermodynamics/isobaricWork.png'
+import firstLawOfThermodynamics from '@/assets/thermodynamics/firstLawOfThermodynamics.png'
+import adiabaticProcess from '@/assets/thermodynamics/adiabaticProcess.png'
+import thermalEfficiency from '@/assets/thermodynamics/thermalEfficiency.png'
+import carnotEfficiency from '@/assets/thermodynamics/carnotEfficiency.png'
+import thermalBalanceEquation from '@/assets/thermodynamics/thermalBalanceEquation.png'
+import reflectionLaw from '@/assets/optics/reflectionLaw.png'
+import refractionLaw from '@/assets/optics/refractionLaw.png'
+import absoluteRefractiveIndex from '@/assets/optics/absoluteRefractiveIndex.png'
+import relativeRefractiveIndex from '@/assets/optics/relativeRefractiveIndex.png'
+import frequencyRatio from '@/assets/optics/frequencyRatio.png'
+import criticalAngle from '@/assets/optics/criticalAngle.png'
+import lensPower from '@/assets/optics/lensPower.png'
+import thinLensFormula from '@/assets/optics/thinLensFormula.png'
+import lensMagnification from '@/assets/optics/lensMagnification.png'
+import interferenceMaxCondition from '@/assets/optics/interferenceMaxCondition.png'
+import interferenceMinCondition from '@/assets/optics/interferenceMinCondition.png'
+import diffractionGratingEquation from '@/assets/optics/diffractionGratingEquation.png'
+import bohrSecondPostulate from '@/assets/atomicAndNuclearPhysics/bohrSecondPostulate.png'
+import hydrogenSpectrumLevels from '@/assets/atomicAndNuclearPhysics/hydrogenSpectrumLevels.png'
+import nuclearMassDefect from '@/assets/atomicAndNuclearPhysics/nuclearMassDefect.png'
+import alphaDecay from '@/assets/atomicAndNuclearPhysics/alphaDecay.png'
+import betaMinusDecay from '@/assets/atomicAndNuclearPhysics/betaMinusDecay.png'
+import betaPlusDecay from '@/assets/atomicAndNuclearPhysics/betaPlusDecay.png'
+import radioactiveDecayLaw from '@/assets/atomicAndNuclearPhysics/radioactiveDecayLaw.png'
+import photonEnergy from '@/assets/quantumPhysics/photonEnergy.png'
+import photonMomentum from '@/assets/quantumPhysics/photonMomentum.png'
+import einsteinPhotoelectricEquation from '@/assets/quantumPhysics/einsteinPhotoelectricEquation.png'
+import photoelectricRedThreshold from '@/assets/quantumPhysics/photoelectricRedThreshold.png'
+import stoppingVoltage from '@/assets/quantumPhysics/stoppingVoltage.png'
+import deBroglieWavelength from '@/assets/quantumPhysics/deBroglieWavelength.png'
+import oscillationKinematics from '@/assets/vibrationsAndWaves/oscillationKinematics.png'
+import harmonicOscillationEq from '@/assets/vibrationsAndWaves/harmonicOscillationEq.png'
+import dynamicOscillationEq from '@/assets/vibrationsAndWaves/dynamicOscillationEq.png'
+import energyConservationOsc from '@/assets/vibrationsAndWaves/energyConservationOsc.png'
+import oscillationPeriod from '@/assets/vibrationsAndWaves/oscillationPeriod.png'
+import pendulumPeriod from '@/assets/vibrationsAndWaves/pendulumPeriod.png'
+import springPendulumPeriod from '@/assets/vibrationsAndWaves/springPendulumPeriod.png'
+import waveSpeed from '@/assets/vibrationsAndWaves/waveSpeed.png'
+import magneticFlux from '@/assets/electromagneticInduction/magneticFlux.png'
+import inducedEMF from '@/assets/electromagneticInduction/inducedEMF.png'
+import motionEMF from '@/assets/electromagneticInduction/motionEMF.png'
+import inductance from '@/assets/electromagneticInduction/inductance.png'
+import selfInducedEMF from '@/assets/electromagneticInduction/selfInducedEMF.png'
+import magneticFieldEnergy from '@/assets/electromagneticInduction/magneticFieldEnergy.png'
 
 // Массивы для категорий
 const staticsAndHydrostatics = [
-    { id: 1, title: "Момент силы", img: momentOfPower },
-    { id: 2, title: "Центр масс системы материальных точек", img: centerOfMassOfTheSystemOfMaterialPoints },
-    { id: 3, title: "Условия равновесия твёрдого тела в ИСО", img: conditionsOfEquilibriumOfSolidBodyInISO },
-    { id: 4, title: "Давление в жидкости", img: liquidPressure },
-    { id: 5, title: "Закон Архимеда", img: archimedesLaw },
-    { id: 6, title: "Модуль силы Архимеда", img: archimedesForceModule },
+    {
+        id: 1,
+        title: "Момент силы", img:
+        momentOfPower
+    },
+    {
+        id: 2,
+        title: "Центр масс системы материальных точек",
+        img: centerOfMassOfTheSystemOfMaterialPoints
+    },
+    {
+        id: 3,
+        title: "Условия равновесия твёрдого тела в ИСО",
+        img: conditionsOfEquilibriumOfSolidBodyInISO
+    },
+    {
+        id: 4,
+        title: "Давление в жидкости",
+        img: liquidPressure
+    },
+    {
+        id: 5,
+        title: "Закон Архимеда",
+        img: archimedesLaw
+    },
+    {
+        id: 6,
+        title: "Модуль силы Архимеда",
+        img: archimedesForceModule
+    },
 ];
 
 const magneticField = [
-    { id: 7, title: "Принцип суперпозиции магнитных полей", img: principleOfSuperpositionOfMagneticFields },
-    { id: 8, title: "Сила Ампера", img: amperePower },
-    { id: 9, title: "Сила Лоренца", img: lorentzForce },
+    {
+        id: 7,
+        title: "Принцип суперпозиции магнитных полей",
+        img: principleOfSuperpositionOfMagneticFields
+    },
+    {
+        id: 8,
+        title: "Сила Ампера",
+        img: amperePower
+    },
+    {
+        id: 9,
+        title: "Сила Лоренца",
+        img: lorentzForce
+    },
 ];
 
 const electromagneticVibrationsAndWaves = [
@@ -264,7 +360,7 @@ const kinematicsMaterialPoint = [
         img: v_total
     },
     {
-        id: 24,
+        id: 42,
         title: "Ускорение материальной точки",
         img: acceleration
     },
@@ -441,28 +537,364 @@ const lawsDirectCurrent = [
     },
 ]
 
+const molecularKineticTheory = [
+    {
+        id: 75,
+        title: "Количество вещества",
+        img: substanceAmount
+    },
+    {
+        id: 76,
+        title: "Основное уравнение МКТ",
+        img: mktEquation
+    },
+    {
+        id: 77,
+        title: "Средняя кинетическая энергия поступательного молекул",
+        img: averageTranslationalEnergy
+    },
+    {
+        id: 78,
+        title: "Уравнение состояния идеального газа в форме Больцмана",
+        img: boltzmannEquation
+    },
+    {
+        id: 79,
+        title: "Уравнение Менделеева-Клапейрона",
+        img: mendeleevClapeyronEquation
+    },
+    {
+        id: 80,
+        title: "Внутренняя энергия одноатомного идеального газа",
+        img: internalEnergyMonoatomicGas
+    },
+    {
+        id: 81,
+        title: "Закон Дальтона для смеси газов",
+        img: daltonLaw
+    },
+    {
+        id: 82,
+        title: "Изотермический процесс",
+        img: isothermalProcess
+    },
+    {
+        id: 83,
+        title: "Изохорный процесс",
+        img: isochoricProcess
+    },
+    {
+        id: 84,
+        title: "Изобарный процесс",
+        img: isobaricProcess
+    },
+    {
+        id: 85,
+        title: "Относительная влажность воздуха",
+        img: relativeHumidity
+    },
+]
+
+const thermodynamics = [
+    {
+        id: 86,
+        title: "Удельная теплоёмкость вещества с",
+        img: specificHeatCapacity
+    },
+    {
+        id: 87,
+        title: "Удельная теплота плавления λ",
+        img: specificHeatOfFusion
+    },
+    {
+        id: 88,
+        title: "Удельная теплота парообразования L",
+        img: lambdaVaporization
+    },
+    {
+        id: 89,
+        title: "Удельная теплота сгорания топлива q",
+        img: specificHeatOfCombustion
+    },
+    {
+        id: 90,
+        title: "Работа газа при изобарном процессе",
+        img: isobaricWork
+    },
+    {
+        id: 91,
+        title: "Первый закон термодинамики",
+        img: firstLawOfThermodynamics
+    },
+    {
+        id: 92,
+        title: "Адиабатный процесс",
+        img: adiabaticProcess
+    },
+    {
+        id: 93,
+        title: "КПД тепловых машин",
+        img: thermalEfficiency
+    },
+    {
+        id: 94,
+        title: "Максимальное значение КПД. Цикл Карно",
+        img: carnotEfficiency
+    },
+    {
+        id: 95,
+        title: "Уравнение теплового баланса",
+        img: thermalBalanceEquation
+    },
+]
+
+const optics = [
+    {
+        id: 96,
+        title: "Закон отражения света",
+        img: reflectionLaw
+    },
+    {
+        id: 97,
+        title: "Закон преломления света",
+        img: refractionLaw
+    },
+    {
+        id: 98,
+        title: "Абсолютный показатель преломления",
+        img: absoluteRefractiveIndex
+    },
+    {
+        id: 99,
+        title: "Относительный показатель преломления",
+        img: relativeRefractiveIndex
+    },
+    {
+        id: 100,
+        title: "Соотношение частот и соотношение длин волн при переходе света из одной среды в другую",
+        img: frequencyRatio
+    },
+    {
+        id: 101,
+        title: "Предельный угол полного внутреннего отражения",
+        img: criticalAngle
+    },
+    {
+        id: 102,
+        title: "Оптическая сила линзы",
+        img: lensPower
+    },
+    {
+        id: 103,
+        title: "Формула тонкой линзы",
+        img: thinLensFormula
+    },
+    {
+        id: 104,
+        title: "Увеличение линзы",
+        img: lensMagnification
+    },
+    {
+        id: 105,
+        title: "Условие максимума интерференции",
+        img: interferenceMaxCondition
+    },
+    {
+        id: 106,
+        title: "Условие минимума интерференции",
+        img: interferenceMinCondition
+    },
+    {
+        id: 107,
+        title: "Формула дифракционной решетки",
+        img: diffractionGratingEquation
+    },
+]
+
+const quantumPhysics = [
+    {
+        id: 108,
+        title: "Энергия фотона",
+        img: photonEnergy
+    },
+    {
+        id: 109,
+        title: "Импульс фотона",
+        img: photonMomentum
+    },
+    {
+        id: 110,
+        title: "Уравнение Эйнштейна для фотоэффекта",
+        img: einsteinPhotoelectricEquation
+    },
+    {
+        id: 111,
+        title: "Красная граница фотоэффекта",
+        img: photoelectricRedThreshold
+    },
+    {
+        id: 112,
+        title: "Запирающее напряжение",
+        img: stoppingVoltage
+    },
+    {
+        id: 113,
+        title: "Длина волны де Бройля движущейся частицы",
+        img: deBroglieWavelength
+    },
+]
+
+const atomicAndNuclearPhysics = [
+    {
+        id: 108,
+        title: "Второй постулат Бора",
+        img: bohrSecondPostulate
+    },
+    {
+        id: 109,
+        title: "Спектр уровней энергии атома водорода",
+        img: hydrogenSpectrumLevels
+    },
+    {
+        id: 110,
+        title: "Дефект массы ядра",
+        img: nuclearMassDefect
+    },
+    {
+        id: 111,
+        title: "Альфа-распад",
+        img: alphaDecay
+    },
+    {
+        id: 112,
+        title: "Электронный β-распад",
+        img: betaMinusDecay
+    },
+    {
+        id: 113,
+        title: "Позитронный β-распад",
+        img: betaPlusDecay
+    },
+    {
+        id: 114,
+        title: "Закон радиоактивного распада",
+        img: radioactiveDecayLaw
+    },
+]
+
+const vibrationsAndWaves = [
+    {
+        id: 115,
+        title: "Кинематика механических колебаний",
+        img: oscillationKinematics
+    },
+    {
+        id: 116,
+        title: "Уравнение гармонических колебаний",
+        img: harmonicOscillationEq
+    },
+    {
+        id: 117,
+        title: "Динамическое описание колебаний",
+        img: dynamicOscillationEq
+    },
+    {
+        id: 118,
+        title: "Закон сохранения энергии при колебаниях",
+        img: energyConservationOsc
+    },
+    {
+        id: 119,
+        title: "Период и частота колебаний",
+        img: oscillationPeriod
+    },
+    {
+        id: 120,
+        title: "Период колебаний математического маятника",
+        img: pendulumPeriod
+    },
+    {
+        id: 114,
+        title: "Период колебаний пружинного маятника",
+        img: springPendulumPeriod
+    },
+    {
+        id: 121,
+        title: "Скорость распространения и длина волны",
+        img: waveSpeed
+    },
+]
+
+const electromagneticInduction = [
+    {
+        id: 122,
+        title: "Поток вектора магнитной индукции",
+        img: magneticFlux
+    },
+    {
+        id: 123,
+        title: "Закон электромагнитной индукции Фарадея",
+        img: inducedEMF
+    },
+    {
+        id: 124,
+        title: "ЭДС индукции в прямом движущимся проводнике",
+        img: motionEMF
+    },
+    {
+        id: 125,
+        title: "Индуктивность",
+        img: inductance
+    },
+    {
+        id: 126,
+        title: "ЭДС самоиндукции",
+        img: selfInducedEMF
+    },
+    {
+        id: 127,
+        title: "Энергия магнитного поля катушки с током",
+        img: magneticFieldEnergy
+    },
+]
+
 // Список всех формул (объединение категорий)
 const allFormulas = [
-    ...staticsAndHydrostatics,
-    ...magneticField,
-    ...electromagneticVibrationsAndWaves,
-    ...dynamics,
-    ...conservationLaws,
     ...kinematicsMaterialPoint,
+    ...dynamics,
+    ...staticsAndHydrostatics,
+    ...conservationLaws,
+    ...vibrationsAndWaves,
+    ...molecularKineticTheory,
+    ...thermodynamics,
     ...electrostatics,
-    ...lawsDirectCurrent
+    ...lawsDirectCurrent,
+    ...magneticField,
+    ...electromagneticInduction,
+    ...electromagneticVibrationsAndWaves,
+    ...optics,
+    ...stoFundamentals,
+    ...quantumPhysics,
+    ...atomicAndNuclearPhysics,
 ];
 
 // Экспорт категорий
 export const formulas = {
     "Все": allFormulas,
-    "Статика и гидростатика": staticsAndHydrostatics,
-    "Магнитное поле": magneticField,
-    "Электромагнитные колебания и волны": electromagneticVibrationsAndWaves,
-    "Динамика": dynamics,
-    "Основы специальной теории относительности (СТО)": stoFundamentals,
-    "Законы сохранения в механике": conservationLaws,
     "Кинематика материальной точки": kinematicsMaterialPoint,
+    "Динамика": dynamics,
+    "Статика и гидростатика": staticsAndHydrostatics,
+    "Законы сохранения в механике": conservationLaws,
+    "Колебания и волны": vibrationsAndWaves,
+    "Молекулярно-кинетическая теория (МКТ)": molecularKineticTheory,
+    "Термодинамика": thermodynamics,
     "Электростатика": electrostatics,
-    "Законы постоянного тока": lawsDirectCurrent
+    "Законы постоянного тока": lawsDirectCurrent,
+    "Магнитное поле": magneticField,
+    "Электромагнитная индукция": electromagneticInduction,
+    "Электромагнитные колебания и волны": electromagneticVibrationsAndWaves,
+    "Оптика": optics,
+    "Основы специальной теории относительности (СТО)": stoFundamentals,
+    "Квантовая физика": quantumPhysics,
+    "Физика атома и атомного ядра": atomicAndNuclearPhysics,
 };
